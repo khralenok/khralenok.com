8284f5a69a519fe62bead5b6060099f09fa71e3a
Grigorii Khralenok <khralenok.g@gmail.com>
2024-11-22 09:33:21 +0700

    test

--- respondForm.php
+++ respondForm.php
@@ -1,27 +1,12 @@
 <?php
-// Get data from form  
-$name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
-$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
-$message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
-
-// Define recipient and subject
 $to = "khralenok.g@gmail.com";
-$subject = "This is the subject line";
+$subject = "Test Email";
+$message = "This is a test email sent using the PHP mail() function.";
+$headers = "From: grigorii@khraleok.com";
 
-// The following text will be sent
-$txt = "Name = $name\r\nEmail = $email\r\nMessage = $message";
-
-// Define headers
-$headers = "From: grigorii@khralenok.com\r\n" .
-           "Reply-To: $email\r\n" .
-           "X-Mailer: PHP/" . phpversion();
-
-// Validate email and send mail
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    mail($to, $subject, $txt, $headers);
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully.";
+} else {
+    echo "Failed to send email.";
 }
-
-// Redirect to another page
-header("Location: index.html");
-exit(); // Ensure no further processing
 ?>
