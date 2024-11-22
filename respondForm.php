2fb106a09d6a6ea1e5e0735c5386a7e1dc2a21a7
Grigorii Khralenok <khralenok.g@gmail.com>
2024-11-22 09:40:19 +0700

    Test of the form

--- respondForm.php
+++ respondForm.php
@@ -1,12 +1,25 @@
 <?php
+// Get data from form  
+$name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+$message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
+
+
+
 $to = "khralenok.g@gmail.com";
-$subject = "Test Email";
-$message = "This is a test email sent using the PHP mail() function.";
+$subject = "New form submission on khralenok.com";
+$message = "Name = $name\r\nEmail = $email\r\nMessage = $message";
 $headers = "From: grigorii@khraleok.com";
+
 
 if (mail($to, $subject, $message, $headers)) {
     echo "Email sent successfully.";
 } else {
     echo "Failed to send email.";
 }
+
+// Redirect to another page
+header("Location: /");
+exit(); // Ensure no further processing
 ?>
+
